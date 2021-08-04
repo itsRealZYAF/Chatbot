b0b863d56ffa0047f308b314a6c0c7a9928d7d02
itsRealZYAF <72516691+itsRealZYAF@users.noreply.github.com>
2021-08-04 15:31:00 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: "", // The name of your bot.
+    channel: "", // ID of the channel where you want the chatbot to speak.
+
+    authorization: "" // Your token to be retrieved from the API site.
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.js");
+
+const keepAlive = require('./keepAlive.js');
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} is ready.\n\n-\n\n`);
+});
+
+client.on("message", (message) => {
+    if (message.author.bot) return
+    if (message.channel.id === config.channel) {
+    const fetch = require("node-fetch").default;
+
+    fetch(`https://api.snowflakedev.org/api/chatbot?message=${encodeURIComponent(message.content)}&name=${config.botname}`, {
+        headers: {
+            "Authorization": config.authorization
+        }
+    }).then(res => res.json()).then(data => {
+        message.channel.send(data.message);
+    }).catch(e => console.error('An error has occurred. Please make sure that you provide the correct information in the configuration file.'));
+    }
+
+console.log(`Message from ${message.author.tag} - ${message.content}`)});
+
+keepAlive();
+
+client.login("YOUR TOKEN").catch(() => console.log("Invalid token."));
