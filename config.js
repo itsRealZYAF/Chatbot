482d23109ba85347f9f3987f91d5f5c26164101a
itsRealZYAF <72516691+itsRealZYAF@users.noreply.github.com>
2021-08-04 10:13:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "", // The name of your bot.
+    token: "", // The bot token to put here.
     channel: "", // ID of the channel where you want the chatbot to speak.
 
-    authorization: "" // Your token to be retrieved from the API site.
+    authorization: "" // Get your token from https://api.snowflakedev.org/ and paste it here.
 }
